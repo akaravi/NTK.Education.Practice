d3db50ca7b0e123d7356d25f1924b4dde9131b6b
karavi <karavi.alireza@gmail.com>
2025-10-09 07:09:24 +0330

    update

--- 3D vision/1-14040710/practic1.m
+++ 3D vision/1-14040710/practic1.m
@@ -2,11 +2,24 @@ clc
 clear
 close all
 img=im2double(imread('rice.png'));
+
+% تعیین ابعاد تصویر
+width = 256;  % عرض تصویر
+height = 256; % ارتفاع تصویر
+
+% ایجاد تصویر سیاه
+black_image = zeros(height, width); 
+
+% ایجاد تصویر با شدت نور 255
+light_image = ones(height, width) * 255; 
+
 gray_image =img;
+%gray_image =black_image;
+%gray_image =light_image;
 %gray_image = rgb2gray(img);
 
 
-dft = fft2(double(gray_image));
+dft = fft2(gray_image);
 dft_shift = fftshift(dft); % جابجایی صفرها به وسط
 magnitude_spectrum = log(abs(dft_shift) + 1); % محاسبه طیف دامنه
 
